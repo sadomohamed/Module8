41f4e43822a7832a1c8d0e777e78a80021fd6a28
sadomohamed <43449772+sadomohamed@users.noreply.github.com>
2018-11-19 14:28:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addItem () {
 var newItem=
-    document.getElementbyId ('addItem')
+    document.getElementbyId ('items').value;
 // Create a new element and store it in a variable.
 var newEl = document.createElement('li');
 
