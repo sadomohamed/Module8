10ed2bfcd282f7e96e6d8b3829ffc4d1216502ac
sadomohamed <43449772+sadomohamed@users.noreply.github.com>
2018-11-19 14:56:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function addItem () {
-var newItem=
-    document.getElementbyId ('items').value;
+function addItem(){
+var newItem =
+    document.getElementById('items').value;
 // Create a new element and store it in a variable.
 var newEl = document.createElement('li');
 
 // Create a text node and store it in a variable.
-var newText = document.createTextNode (newItem);
- 
- 
+var newText = document.createTextNode(newItem);
+
 // Attach the new text node to the new element.
-newEl.appendChild('newtext')
+newEl.appendChild(newText);
+
 // Find the position where the new element should be added.
 var position = document.getElementsByTagName('ul')[0];
 
